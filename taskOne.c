65a1a74ea9dbbd595217a5fa2a709d59103565c4
Daniel Milligan <ddmilligan@cpp.edu>
2022-03-01 14:48:43 -0800

    Add outline for task 1

---
+++ taskOne.c
@@ -0,0 +1,16 @@
+// Import relevant libraries
+
+// Define a main method with a header such that two parameters are accepted.
+
+    // Validate that the correct number and types of command line arguments
+    // have been passed into main. Terminate with an error if user input is bad.
+
+    // Generate a random integer in [0, 3] to determine meal choice.
+
+    // Define variables for the food name and price based on the random integer.
+
+    // Calculate the tax amount, tip amount, and total bill.
+
+    // Output all information to the terminal.
+
+    // Terminate program.
