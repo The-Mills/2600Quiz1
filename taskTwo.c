325d97b35362b04fe9c396504f7577da7eebd62e
Daniel Milligan <ddmilligan@cpp.edu>
2022-03-01 17:31:20 -0800

    Add outline for Task 2

---
+++ taskTwo.c
@@ -0,0 +1,50 @@
+// Import relevant libraries
+
+// Define global variables if necessary.
+
+// Define a main function
+
+   // Define all variables that will be used throughout the program.
+
+   // Place the game in an infinite loop
+
+      // Display menu
+
+      // Collect and validate user input
+
+      // Execute different instructions based on
+      // the input value.
+
+         // Input value '1' --> execute playGame() function
+         // Input value '2' --> execute changeMaxValue() function
+         // Input value '3' --> break from the main game loop and run termination code
+
+    // Terminate program
+
+// Define a playGame() function
+   
+   // Define all relevant local variables
+
+   // Ask the user to input a number
+
+   // Collect and validate user input
+
+   // Execute different instructions based on the input value
+      
+        // If the input value is 'q' --> mark the game as a loss and return
+
+        // If the input value is a number, compare it with the correct number
+    
+            // If the guess is correct, store the game's data, congratulate
+            // the player, then return.
+
+            // If the game is a loss, inform the user and prompt for another guess.
+
+
+// Define a changeMaxValue() function
+
+    // Tell the user what the largest value possible is.
+
+    // Collect and validate user input.
+
+    // If the number is valid, replace the max value with the user's input.
